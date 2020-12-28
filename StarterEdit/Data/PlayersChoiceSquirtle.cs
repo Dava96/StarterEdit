2a8e926ac5faebf06ce476f23a6ff7b3f6693b95
David <Davidlomath@hotmail.co.uk>
2020-12-28 09:40:47 +0000

    Fixed whitespace

--- StarterEdit/Data/PlayersChoiceSquirtle.cs
+++ StarterEdit/Data/PlayersChoiceSquirtle.cs
@@ -6,10 +6,8 @@ namespace StarterEdit
 {
     class PlayersChoiceSquirtle
     {
-
         public long[] squirtleBattle1Pkm = new long[] { 0x3a1F5, 0x3a1F7, 0x0, 0x0, 0x0, 0x0 };  // first battle with the rival when the players choice is Squirtle. 
         public long[] squirtleBattle1Lvl = new long[] { 0x3a1F4, 0x3a1F6, 0x0, 0x0, 0x0, 0x0 }; // level array
-
 
         public long[] squirtleBattle2Pkm = new long[] { 0x3a20B, 0x3a20D, 0x3a20F, 0x3a211, 0x0, 0x0 };
         public long[] squirtleBattle2Lvl = new long[] { 0x3a20A, 0x3a20C, 0x3a20E, 0x3a210, 0x0, 0x0 };
