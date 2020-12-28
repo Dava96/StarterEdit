af23c947c8b75e437056911193145e135af381d5
David <Davidlomath@hotmail.co.uk>
2020-12-28 08:53:51 +0000

    Added 2nd battle offsets for Bulbasuar and Charmander

--- StarterEdit/Data/PlayersChoiceBulbasaur.cs
+++ StarterEdit/Data/PlayersChoiceBulbasaur.cs
@@ -9,5 +9,8 @@ namespace StarterEdit
         public long[] bulbasaurBattle1Pkm = new long[] { 0x3a1FB, 0x3a1FD, 0x0, 0x0, 0x0, 0x0 }; // first battle with the rival when the players choice is bulbasuar. 
         public long[] bulbasurBattle1Lvl = new long[] { 0x3a1FA, 0x3a1FC, 0x0, 0x0, 0x0, 0x0 }; // level array
 
+        public long[] bulbasaurBattle2Pkm = new long[] { 0x3a215, 0x3a217, 0x3a219, 0x3a21B, 0x0, 0x0 }; // first battle with the rival when the players choice is bulbasuar. 
+        public long[] bulbasurBattle2Lvl = new long[] { 0x3a214, 0x3a216, 0x3a218, 0x3a21A, 0x0, 0x0 }; // level array
+
     }
 }
--- StarterEdit/Data/PlayersChoiceCharmander.cs
+++ StarterEdit/Data/PlayersChoiceCharmander.cs
@@ -10,5 +10,8 @@ namespace StarterEdit
         public long[] charmanderBattle1Pkm = new long[] { 0x3a1EF, 0x3a1F1, 0x0, 0x0, 0x0, 0x0 }; // first battle with the rival when the players choice is charmander. 
         public long[] charmanderBattle1Lvl = new long[] { 0x3a1EE, 0x3a1F0, 0x0, 0x0, 0x0, 0x0 }; // level array
 
+        public long[] charmanderBattle2Pkm = new long[] { 0x3a201, 0x3a203, 0x3a205, 0x3a207, 0x0, 0x0 }; // first battle with the rival when the players choice is charmander. 
+        public long[] charmanderBattle2lvl = new long[] { 0x3a200, 0x3a202, 0x3a204, 0x3a206, 0x0, 0x0 }; // level array
+
     }
 }
