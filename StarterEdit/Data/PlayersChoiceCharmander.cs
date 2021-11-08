84e0a0317823c24696581af1d448b22247ec3cde
David <Davidlomath@hotmail.co.uk>
2021-11-08 13:05:14 +0000

    Changed Variable names to make them more descriptive

--- StarterEdit/Data/PlayersChoiceCharmander.cs
+++ StarterEdit/Data/PlayersChoiceCharmander.cs
@@ -6,25 +6,25 @@ namespace StarterEdit
 {
     class PlayersChoiceCharmander
     {
-        public long[] charmanderBattle1Pkm = new long[] { 0x3a1EF, 0x3a1F1, 0x0, 0x0, 0x0, 0x0 }; // first battle with the rival when the players choice is charmander. 
-        public long[] charmanderBattle1Lvl = new long[] { 0x3a1EE, 0x3a1F0, 0x0, 0x0, 0x0, 0x0 }; // level array
+        public long[] charmanderBattleRoute22Pkm = new long[] { 0x3a1EF, 0x3a1F1, 0x0, 0x0, 0x0, 0x0 }; // first battle with the rival when the players choice is charmander. 
+        public long[] charmanderBattleRoute22Lvl = new long[] { 0x3a1EE, 0x3a1F0, 0x0, 0x0, 0x0, 0x0 }; // level array
 
-        public long[] charmanderBattle2Pkm = new long[] { 0x3a201, 0x3a203, 0x3a205, 0x3a207, 0x0, 0x0 };
-        public long[] charmanderBattle2lvl = new long[] { 0x3a200, 0x3a202, 0x3a204, 0x3a206, 0x0, 0x0 };
+        public long[] charmanderBattleCeruleanCityPkm = new long[] { 0x3a201, 0x3a203, 0x3a205, 0x3a207, 0x0, 0x0 };
+        public long[] charmanderBattleCeruleanCityLvl = new long[] { 0x3a200, 0x3a202, 0x3a204, 0x3a206, 0x0, 0x0 };
 
-        public long[] charmanderBattle3Pkm = new long[] { 0x3a403, 0x3a405, 0x3a407, 0x3a409, 0x0, 0x0 };
-        public long[] charmanderBattle3lvl = new long[] { 0x3a402, 0x3a404, 0x3a406, 0x3a408, 0x0, 0x0 };
+        public long[] charmanderBattleSSAnnePkm = new long[] { 0x3a403, 0x3a405, 0x3a407, 0x3a409, 0x0, 0x0 };
+        public long[] charmanderBattleSSAnneLvl = new long[] { 0x3a402, 0x3a404, 0x3a406, 0x3a408, 0x0, 0x0 };
 
-        public long[] charmanderBattle4Pkm = new long[] { 0x3a421, 0x3a423, 0x3a425, 0x3a427, 0x3a429, 0x0 };
-        public long[] charmanderBattle4lvl = new long[] { 0x3a420, 0x3a422, 0x3a424, 0x3a426, 0x3a428, 0x0 };
+        public long[] charmanderBattlePokemonTowerPkm = new long[] { 0x3a421, 0x3a423, 0x3a425, 0x3a427, 0x3a429, 0x0 };
+        public long[] charmanderBattlePokemonTowerLvl = new long[] { 0x3a420, 0x3a422, 0x3a424, 0x3a426, 0x3a428, 0x0 };
 
-        public long[] charmanderBattle5Pkm = new long[] { 0x3a445, 0x3a447, 0x3a449, 0x3a44B, 0x3a44D, 0x0 };
-        public long[] charmanderBattle5lvl = new long[] { 0x3a444, 0x3a446, 0x3a448, 0x3a44A, 0x3a44C, 0x0 };
+        public long[] charmanderBattleSilphCoPkm = new long[] { 0x3a445, 0x3a447, 0x3a449, 0x3a44B, 0x3a44D, 0x0 };
+        public long[] charmanderBattleSilphCoLvl = new long[] { 0x3a444, 0x3a446, 0x3a448, 0x3a44A, 0x3a44C, 0x0 };
 
-        public long[] charmanderBattle6Pkm = new long[] { 0x3a469, 0x3a46B, 0x3a46D, 0x3a46F, 0x3a44D, 0x3a471 };
-        public long[] charmanderBattle6lvl = new long[] { 0x3a468, 0x3a46A, 0x3a46C, 0x3a46E, 0x3a44C, 0x3a470 };
+        public long[] charmanderBattleRoute22Pkm2 = new long[] { 0x3a469, 0x3a46B, 0x3a46D, 0x3a46F, 0x3a44D, 0x3a471 };
+        public long[] charmanderBattleRoute22Lvl2 = new long[] { 0x3a468, 0x3a46A, 0x3a46C, 0x3a46E, 0x3a44C, 0x3a470 };
 
-        public long[] charmanderBattle7Pkm = new long[] { 0x3a493, 0x3a495, 0x3a497, 0x3a499, 0x3a49B, 0x3a49D };
-        public long[] charmanderBattle7lvl = new long[] { 0x3a492, 0x3a494, 0x3a496, 0x3a498, 0x3a49A, 0x3a49C };
+        public long[] charmanderBattleIndigoPlateauPkm = new long[] { 0x3a493, 0x3a495, 0x3a497, 0x3a499, 0x3a49B, 0x3a49D };
+        public long[] charmanderBattleIndigoPlateauLvl = new long[] { 0x3a492, 0x3a494, 0x3a496, 0x3a498, 0x3a49A, 0x3a49C };
     }
 }
