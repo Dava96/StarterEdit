5bb8de27f12143ccaa29953024ea3250365b9e05
David <Davidlomath@hotmail.co.uk>
2021-11-08 14:49:50 +0000

    Corrected wrong offset causing it to read and write in the incorrect place

--- StarterEdit/Data/PlayersChoiceBulbasaur.cs
+++ StarterEdit/Data/PlayersChoiceBulbasaur.cs
@@ -12,8 +12,8 @@ namespace StarterEdit
         public long[] bulbasaurBattleCeruleanCityPkm = new long[] { 0x3a215, 0x3a217, 0x3a219, 0x3a21B, 0x0, 0x0 }; 
         public long[] bulbasaurBattleCeruleanCityLvl = new long[] { 0x3a214, 0x3a216, 0x3a218, 0x3a21A, 0x0, 0x0 };
 
-        public long[] bulbasaurBattleSSAnnePkm = new long[] { 0x3a418, 0x3a41A, 0x3a41C, 0x3a21B, 0x0, 0x0 };
-        public long[] bulbasaurBattleSSAnneLvl = new long[] { 0x3a416, 0x3a419, 0x3a41B, 0x3a41D, 0x0, 0x0 };
+        public long[] bulbasaurBattleSSAnnePkm = new long[] { 0x3A403, 0x3A405, 0x3A407, 0x3A409, 0x0, 0x0 };
+        public long[] bulbasaurBattleSSAnneLvl = new long[] { 0x3A402, 0x3A404, 0x3A406, 0x3A408, 0x0, 0x0 };
 
         public long[] bulbasaurBattlePokemonTowerPkm = new long[] { 0x3a439, 0x3a43B, 0x3a43D, 0x3a43F, 0x3a441, 0x0 };
         public long[] bulbasaurBattlePokemonTowerLvl = new long[] { 0x3a438, 0x3a43A, 0x3a43C, 0x3a43E, 0x3a440, 0x0 };
@@ -26,6 +26,5 @@ namespace StarterEdit
 
         public long[] bulbasaurBattleIndigoPlateauPkm = new long[] { 0x3a4AF, 0x3a4B1, 0x3a4B3, 0x3a4B5, 0x3a4B7, 0x3a4B9 };
         public long[] bulbasaurBattleIndigoPlateauLvl = new long[] { 0x3a4AE, 0x3a4B0, 0x3a4B2, 0x3a4B4, 0x3a4B6, 0x3a4B8 };
-
     }
 }
