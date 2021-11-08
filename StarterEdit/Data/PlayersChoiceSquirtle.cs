2868fefde1f1e2de7ea822edabb7c64c50297d7a
David <Davidlomath@hotmail.co.uk>
2021-11-08 13:05:17 +0000

    Changed Variable names to make them more descriptive

--- StarterEdit/Data/PlayersChoiceSquirtle.cs
+++ StarterEdit/Data/PlayersChoiceSquirtle.cs
@@ -6,26 +6,26 @@ namespace StarterEdit
 {
     class PlayersChoiceSquirtle
     {
-        public long[] squirtleBattle1Pkm = new long[] { 0x3a1F5, 0x3a1F7, 0x0, 0x0, 0x0, 0x0 };  // first battle with the rival when the players choice is Squirtle. 
-        public long[] squirtleBattle1Lvl = new long[] { 0x3a1F4, 0x3a1F6, 0x0, 0x0, 0x0, 0x0 }; // level array
+        public long[] squirtleBattleRoute22Pkm = new long[] { 0x3a1F5, 0x3a1F7, 0x0, 0x0, 0x0, 0x0 };  // first battle with the rival when the players choice is Squirtle. 
+        public long[] squirtleBattleRoute22Lvl = new long[] { 0x3a1F4, 0x3a1F6, 0x0, 0x0, 0x0, 0x0 }; // level array
 
-        public long[] squirtleBattle2Pkm = new long[] { 0x3a20B, 0x3a20D, 0x3a20F, 0x3a211, 0x0, 0x0 };
-        public long[] squirtleBattle2Lvl = new long[] { 0x3a20A, 0x3a20C, 0x3a20E, 0x3a210, 0x0, 0x0 };
+        public long[] squirtleBattleCeruleanCityPkm = new long[] { 0x3a20B, 0x3a20D, 0x3a20F, 0x3a211, 0x0, 0x0 };
+        public long[] squirtleBattleCeruleanCityLvl  = new long[] { 0x3a20A, 0x3a20C, 0x3a20E, 0x3a210, 0x0, 0x0 };
 
-        public long[] squirtleBattle3Pkm = new long[] { 0x3a40D, 0x3a40F, 0x3a411, 0x3a413, 0x0, 0x0 };
-        public long[] squirtleBattle3Lvl = new long[] { 0x3a40C, 0x3a40E, 0x3a410, 0x3a412, 0x0, 0x0 };
+        public long[] squirtleBattleSSAnnePkm = new long[] { 0x3a40D, 0x3a40F, 0x3a411, 0x3a413, 0x0, 0x0 };
+        public long[] squirtleBattleSSAnneLvl = new long[] { 0x3a40C, 0x3a40E, 0x3a410, 0x3a412, 0x0, 0x0 };
 
-        public long[] squirtleBattle4Pkm = new long[] { 0x3a42D, 0x3a42F, 0x3a431, 0x3a433, 0x3a435, 0x0 };
-        public long[] squirtleBattle4Lvl = new long[] { 0x3a42C, 0x3a42E, 0x3a430, 0x3a432, 0x3a434, 0x0 };
+        public long[] squirtleBattlePokemonTowerPkm = new long[] { 0x3a42D, 0x3a42F, 0x3a431, 0x3a433, 0x3a435, 0x0 };
+        public long[] squirtleBattlePokemonTowerLvl = new long[] { 0x3a42C, 0x3a42E, 0x3a430, 0x3a432, 0x3a434, 0x0 };
 
-        public long[] squirtleBattle5Pkm = new long[] { 0x3a451, 0x3a453, 0x3a455, 0x3a457, 0x3a459, 0x0 };
-        public long[] squirtleBattle5Lvl = new long[] { 0x3a450, 0x3a452, 0x3a454, 0x3a456, 0x3a458, 0x0 };
+        public long[] squirtleBattleSilphCoPkm = new long[] { 0x3a451, 0x3a453, 0x3a455, 0x3a457, 0x3a459, 0x0 };
+        public long[] squirtleBattleSilphCoLvl = new long[] { 0x3a450, 0x3a452, 0x3a454, 0x3a456, 0x3a458, 0x0 };
 
-        public long[] squirtleBattle6Pkm = new long[] { 0x3a477, 0x3a479, 0x3a47B, 0x3a47D, 0x3a47F, 0x3a481 };
-        public long[] squirtleBattle6Lvl = new long[] { 0x3a476, 0x3a478, 0x3a47A, 0x3a47C, 0x3a47E, 0x3a480 };
+        public long[] squirtleBattleRoute22Pkm2 = new long[] { 0x3a477, 0x3a479, 0x3a47B, 0x3a47D, 0x3a47F, 0x3a481 }; // 2nd Route 22 battle
+        public long[] squirtleBattleRoute22Lvl2 = new long[] { 0x3a476, 0x3a478, 0x3a47A, 0x3a47C, 0x3a47E, 0x3a480 };
 
-        public long[] squirtleBattle7Pkm = new long[] { 0x3a4A1, 0x3a4A3, 0x3a4A5, 0x3a4A7, 0x3a4A9, 0x3a4AB };
-        public long[] squirtleBattle7Lvl = new long[] { 0x3a4A0, 0x3a4A2, 0x3a4A4, 0x3a4A6, 0x3a4A8, 0x3a4AA };
+        public long[] squirtleBattleIndigoPlateauPkm = new long[] { 0x3a4A1, 0x3a4A3, 0x3a4A5, 0x3a4A7, 0x3a4A9, 0x3a4AB };
+        public long[] squirtleBattleIndigoPlateauLvl = new long[] { 0x3a4A0, 0x3a4A2, 0x3a4A4, 0x3a4A6, 0x3a4A8, 0x3a4AA };
 
     }
 }
